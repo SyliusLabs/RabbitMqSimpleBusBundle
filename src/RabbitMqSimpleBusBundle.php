60b1cd47b772d549796423e226686bab2677a13f
Kamil Kokot <kamil@kokot.me>
2017-05-15 14:31:38 +0200

    Register denormalizers by searching for tagged services

--- src/RabbitMqSimpleBusBundle.php
+++ src/RabbitMqSimpleBusBundle.php
@@ -2,8 +2,17 @@
 
 namespace SyliusLabs\RabbitMqSimpleBusBundle;
 
+use SyliusLabs\RabbitMqSimpleBusBundle\DependencyInjection\Compiler\RegisterDenormalizersPass;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 final class RabbitMqSimpleBusBundle extends Bundle
 {
+    /**
+     * {@inheritdoc}
+     */
+    public function build(ContainerBuilder $container)
+    {
+        $container->addCompilerPass(new RegisterDenormalizersPass());
+    }
 }
